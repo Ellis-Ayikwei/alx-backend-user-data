6dfff840d629a45fd49bf68771b30ff5d7c9fb22
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 22:02:53 +0000

    Refactor filter_datum function to improve code readability and maintainability

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,11 +2,11 @@
 import re
 from typing import List
 """Defines a function the retunr an obfuscated msg"""
-
+regmat = r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)"
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ returns the log message obfuscated """
-    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
-                  lambda match: match.group(1) + "=" + redaction
+    return re.sub(regmat,lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
+    
