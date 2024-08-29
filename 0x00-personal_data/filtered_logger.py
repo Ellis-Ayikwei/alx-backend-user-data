0d9918f0038f531024053e52dd59d114d0bb30b4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 20:57:06 +0000

    Refactor filter_datum function for improved code readability and maintainability

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     return re.sub(r'(?<={sep})({flds})=(.*?)(?={sep}|$)'
                   .format(sep=re.escape(separator), flds='|'.join(fields)),
