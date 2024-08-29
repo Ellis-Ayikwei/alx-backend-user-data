7a2e2924c3ce5287c706bbacc707c0a4f4c859b5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 22:44:33 +0000

    Refactor RedactingFormatter class to remove unnecessary whitespace

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
     
 
-    def __init__(self, fields):
+    def __init__(self, fields: List):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields=fields
         
