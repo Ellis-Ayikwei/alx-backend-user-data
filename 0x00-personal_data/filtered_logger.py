0e8bc26a36071a33b09b39b73dbf5430aaf6c96c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 20:58:42 +0000

    Refactor filter_datum function to improve code readability and maintainability

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 import re
+"""Defines a function the retunr an obfuscated message"""
 
 
 def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
     return re.sub(r'(?<={sep})({flds})=(.*?)(?={sep}|$)'
                   .format(sep=re.escape(separator), flds='|'.join(fields)),
                   lambda m: f'{m.group(1)}={redaction}', message)
