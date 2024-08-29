0769861ca99b9bf1892c248c589759c10c2594ed
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 20:56:26 +0000

    the filter datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 import re
 
 def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'(?<={sep})({flds})=(.*?)(?={sep}|$)'.format(sep=re.escape(separator), flds='|'.join(fields)),
+    return re.sub(r'(?<={sep})({flds})=(.*?)(?={sep}|$)'
+                  .format(sep=re.escape(separator), flds='|'.join(fields)),
                   lambda m: f'{m.group(1)}={redaction}', message)
