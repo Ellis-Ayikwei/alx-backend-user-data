7532a81b5533f1d5e5e659b3f63350603f2eca1f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-29 23:31:04 +0000

    Refactor PII_FIELDS tuple to use lowercase field names

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@
 import re
 import logging
 from typing import List
+import mysql.connector
+import os
 regmat = r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)"
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -51,3 +53,16 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database."""
+
+    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST")
+
+    return mysql.connector.connect(
+        user=db_username,
+        password=db_password,
+        host=db_host,
+    )
