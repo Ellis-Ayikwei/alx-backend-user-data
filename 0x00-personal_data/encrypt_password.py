007d5b8580cb90f9d683a9bfa8db99ac2f0bbc59
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-30 12:25:16 +0000

    Refactor get_db function to include bcrypt password hashing and password validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns a hashed password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Returns True if the password is valid."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
