b6a9b203b672c171f92ba4022a1eb0c7f35068c0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-30 12:52:40 +0000

    Refactor get_db function to use MySQLConnection for database connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,14 +58,14 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> connection:
+def get_db() -> connection.MySQLConnection:
     """Creates a connector to a database.
     """
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    conn = mysql.connector.connect(
+    conn = connection.MySQLConnection(
         host=db_host,
         user=db_user,
         password=db_pwd,
