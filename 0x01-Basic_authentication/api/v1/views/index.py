6748119a7c9a34a54379337196a7304777273576
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 16:49:00 +0000

    Refactor unauthorized route to handle 401 errors and improve error handling in get_db function

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""Module of Index views
+This module defines the index views for the API, including routes for checking
+the status, retrieving statistics, and handling unauthorized and
+forbidden requests.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
+from models.user import User
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def status():
+    """GET /api/v1/status
     Return:
-      - the status of the API
+      - the status of the API as a JSON object.
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
+@app_views.route("/stats/", strict_slashes=False)
+def stats():
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each object type in the system as a JSON object.
     """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
+    stats = {"users": User.count()}
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
+def unauthorized() -> str:
+    """GET /unauthorized"""
+    abort(401)
+
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def forbidden():
+    """GET /forbidden
+    Return:
+        - Triggers a 403 Forbidden error.
+    """
+    abort(403)
