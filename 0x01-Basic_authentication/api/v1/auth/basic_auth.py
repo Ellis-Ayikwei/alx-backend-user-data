b3310ff01cff4a39a5fa3e25117696e4538bd5fe
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 23:03:03 +0000

    Refactor current_user method to improve code organization and readability

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -90,3 +90,14 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+    
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user
+        """
+        auth_header = self.authorization_header(request)
+        base64_auth = self.extract_base64_authorization_header(auth_header)
+        decoded_auth = self.decode_base64_authorization_header(base64_auth)
+        user_credentials = self.extract_user_credentials(decoded_auth)
+        return self.user_object_from_credentials(*user_credentials)
