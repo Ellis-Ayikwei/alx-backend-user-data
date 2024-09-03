44fe53dd1942891dd2771f30c275117c1a45a42d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 12:26:18 +0000

    Refactor unauthorized route to handle 401 errors

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,7 +28,7 @@ def stats() -> str:
 
 
 @app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
-def get_unauthorized() -> None:
+def unauthorized() -> None:
     """GET /unauthorized
 
     Return:
