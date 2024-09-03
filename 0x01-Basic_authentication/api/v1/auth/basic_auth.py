15e21cc0d532ae49b51a75693504251bc34990cc
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 22:39:09 +0000

    Refactor user_object_from_credentials method to use User class and improve code organization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -81,7 +81,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         users = User.search({"email": user_email})
-        if len(users) == 0:
+        if not users:
             return None
         for user in users:
             if user.is_valid_password(user_pwd):
