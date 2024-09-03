b3afe3d3778f095fcfd8085bfbd35b4bff5821e5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 16:46:32 +0000

    the file changed to its prvious form

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import Dict, Tuple, Literal
 
 
 app = Flask(__name__)
