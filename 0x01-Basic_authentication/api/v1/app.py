41cdeb8da2b21d7863c2de0cbcac2263d863e25f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 12:52:45 +0000

    chore: Refactor authentication module and unauthorized route to handle 401 errors and improve error handling in get_db function

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+This module sets up the Flask application, including routes,
+authentication, and error handling for the API.
 """
+
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, abort, url_for
-from flask_cors import CORS, cross_origin
-import os
+from flask import Flask, jsonify, abort, request, url_for
+from flask_cors import CORS
 from api.v1.auth.auth import Auth
-from api.v1.views.index import get_forbidden
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views.index import get_forbidden
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if os.getenv("AUTH_TYPE"):
-    if os.getenv("AUTH_TYPE") == "basic_auth":
+if getenv("AUTH_TYPE"):
+    if getenv("AUTH_TYPE") == "basic_auth":
         auth = BasicAuth()
     else:
         auth = Auth()
-
 
 @app.before_request
 def check_auth() -> None:
@@ -45,24 +46,20 @@ def check_auth() -> None:
     if auth.current_user(request) is None:
         abort(403)
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorised(error) -> str:
-    """Unauthorised handler"""
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
-
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
