7d1d1015758db51b1c1daab553323ab114e27f4d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 21:09:39 +0000

    Refactor authorization_header method to include type annotation for request parameter

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,6 @@ class Auth:
         if request is None or "Authorization" not in request.headers:
             return None
         return request.headers["Authorization"]
-        return None
 
     def current_user(self, request=None) -> TypeVar("User"):
         """
