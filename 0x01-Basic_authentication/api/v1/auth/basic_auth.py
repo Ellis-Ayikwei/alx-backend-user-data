526879764a64e51ac6aea733ff881095982b5c77
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 22:17:54 +0000

    Refactor user_object_from_credentials method to use TypeVar for User type hint

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,17 +72,18 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
-    ) -> TypeVar('User'):
+    ) -> TypeVar(User):
         """
         Returns the User object based on the email and password.
         """
-        if not isinstance(user_email, str) or not isinstance(password, str):
+        if user_email is None or not isinstance(user_email, str):
             return None
-
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
         users = User.search({"email": user_email})
-
+        if len(users) == 0:
+            return None
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
-
         return None
