dff2541a445681f8666a821003a234bc2affdea8
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 15:50:54 +0000

    chore: Refactor unauthorized route to handle 401 errors and improve error handling in get_db function

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from flask_cors import CORS
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views.index import get_forbidden
+from typing import Dict
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -55,7 +56,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error: Exception) -> tuple:
+def unauthorized(error: Exception) -> Dict[str, str]:
     """Handles 401 Unauthorized errors.
 
     Returns:
