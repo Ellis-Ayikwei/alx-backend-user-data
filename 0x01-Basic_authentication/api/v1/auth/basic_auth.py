93ae1906f7e23a7ad5096ce7243ea69bcca962e0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 21:34:27 +0000

    Refactor authentication handling to use BasicAuth class and improve code organization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,21 +54,21 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    # def extract_user_credentials(
-    #     self, decoded_base64_authorization_header: str
-    # ) -> (str, str):
-    #     """Returns the user email and password from the decoded Base64
-    #     string
-    #     """
-    #     if decoded_base64_authorization_header is None:
-    #         return None, None
-    #     if not isinstance(decoded_base64_authorization_header, str):
-    #         return None, None
-    #     if ":" not in decoded_base64_authorization_header:
-    #         return None, None
-    #     user_email = decoded_base64_authorization_header.split(":")[0]
-    #     user_name = decoded_base64_authorization_header.split(":")[1]
-    #     return user_email, user_name
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Returns the user email and password from the decoded Base64
+        string
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user_email = decoded_base64_authorization_header.split(":")[0]
+        user_name = decoded_base64_authorization_header.split(":")[1]
+        return user_email, user_name
 
     # def user_object_from_credentials(
     #     self, user_email: str, user_pwd: str
