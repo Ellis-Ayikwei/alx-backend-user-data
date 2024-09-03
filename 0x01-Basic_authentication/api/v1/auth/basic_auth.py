e7cbed6b54a58f0f40c81c2a671ba420932c6217
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 21:35:44 +0000

    Refactor authentication handling to use BasicAuth class and improve code organization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,11 +70,11 @@ class BasicAuth(Auth):
         user_name = decoded_base64_authorization_header.split(":")[1]
         return user_email, user_name
 
-    # def user_object_from_credentials(
-    #     self, user_email: str, user_pwd: str
-    # ) -> TypeVar("User"):
-    #     if user_email is None or not isinstance(user_email, str):
-    #         return None
-    #     if user_pwd is None or not isinstance(user_pwd, str):
-    #         return None
-    #     return None
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        return None
