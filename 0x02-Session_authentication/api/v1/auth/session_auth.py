86a4ae7fd2d54c853172529e2833fc21e1f8831f
Ellis-Ayikwei <126599954+Ellis-Ayikwei@users.noreply.github.com>
2024-09-08 13:32:48 +0000

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,6 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = str(uuid.uuid4())
+    
         self.user_id_by_session_id['session_id'] = user_id
-    pass
+        return self.user_id_session_id['session_id']
