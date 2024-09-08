11bc6c1d7dc9e0523efea6ba2c742bb4d5f1a983
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-08 11:54:04 +0000

    pycodetyle formatted the users.py file

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -35,19 +35,20 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    
+
     if hasattr(request, 'current_user') and request.current_user is not None:
         if user_id == request.current_user.id:
             return jsonify(request.current_user.to_json())
-    
+
     user = User.get(user_id)
     if user is None:
         abort(404)
-    
+
     return jsonify(user.to_json())
 
 
-@app_views.route("/users/<user_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """DELETE /api/v1/users/:id
     Path parameter:
