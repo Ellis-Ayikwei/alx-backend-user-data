0176edb006bb1d22261a9cf49f5974fd0a65c422
Ellis-Ayikwei <126599954+Ellis-Ayikwei@users.noreply.github.com>
2024-09-08 13:36:04 +0000

    Update session_auth.py
    Added a docstring to the function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,8 +10,9 @@ class SessionAuth(Auth):
     
     
     def create_session(self, user_id: str = None) -> str:
+        """ creates a session id for the user id """
         if user_id is None or not isinstance(user_id, str):
             return None
-    
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id['session_id'] = user_id
-        return self.user_id_session_id['session_id']
+        return session_id
