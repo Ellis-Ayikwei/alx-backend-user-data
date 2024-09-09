fa73c2e8f9dce565bfab12879ac14efd9f9f07c6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-09 11:08:42 +0000

    Refactor session_auth.py to handle session authentication and add logout endpoint

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,3 +35,15 @@ def session_login() -> jsonify:
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                     strict_slashes=False)
+def logout() -> jsonify:
+    """Handles the logout route"""
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        abort(404)
