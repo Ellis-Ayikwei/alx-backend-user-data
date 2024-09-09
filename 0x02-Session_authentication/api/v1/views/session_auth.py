e437f428f687d63f03bf8681d8c8446eae44412f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-09 11:04:20 +0000

    Refactor session_auth.py to handle session authentication and fix import formatting

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,8 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-import  os
+import os
+
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login() -> jsonify:
