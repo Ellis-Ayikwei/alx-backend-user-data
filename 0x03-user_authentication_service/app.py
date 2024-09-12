b23c36404679dc19fa1846a5f7ba273d8af885b7
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-12 17:04:59 +0000

    added the profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Defines A"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, redirect, url_for
 from auth import Auth
 from flask import abort
 from auth import _generate_uuid
+
 AUTH = Auth()
 
 app = Flask(__name__)
@@ -26,8 +27,40 @@ def users():
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login():
-    if not request.form['email'] and not request.form['password']:
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email and password:
+        user = AUTH.valid_login(email, password)
+        if user:
+            session_id = _generate_uuid()
+            AUTH.create_session(session_id)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
         abort(401)
-    
+    abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for("index"))
+    return make_response("", 403)
+
+
+@app.route("/profile", methods=["GET"], strict_slahes=False)
+def profile():
+    session_id = request.cookies.get("session_id")
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+    except Exception:
+        return make_response("", 403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,21 @@ class Auth:
     def _generate_uuid() -> str:
         """Return a string of the UUID"""
         return str(uuid.uuid4())
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID."""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session for a user."""
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except Exception:
+            return None
+        
+    
