84cbad5c00d6f266a6abb268f1c78d89b3d0145e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:20:22 +0000

    refactore the code in the pp.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,7 +55,8 @@ def logout():
             user = AUTH.get_user_from_session_id(session_id)
             if user:
                 AUTH.destroy_session(user.id)
-        return redirect("/")
+                return redirect("/")
+        return make_response("", 403)
     except Exception:
         return make_response("", 403)
 
