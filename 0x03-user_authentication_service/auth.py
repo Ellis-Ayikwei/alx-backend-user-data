1c0f0bb58106b416bb82febdf381b75df3a2c66d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 16:16:15 +0000

    reformated to the pycodetsyle standard

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,11 +97,9 @@ class Auth:
             return None
         except Exception:
             return None
-        
-           
+
     def get_reset_password_token(self, email: str) -> str:
-        """Generates a password reset token for a user.
-        """
+        """Generates a password reset token for a user."""
         user = None
         try:
             user = self._db.find_user_by(email=email)
@@ -114,8 +112,7 @@ class Auth:
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates a user's password given the user's reset token.
-        """
+        """Updates a user's password given the user's reset token."""
         user = None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
