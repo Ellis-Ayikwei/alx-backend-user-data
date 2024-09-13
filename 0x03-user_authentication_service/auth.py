aa696dd5e5a26e523410b8f7d8547f6aa8820feb
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 13:17:42 +0000

    added exception handling to the get reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,12 +98,14 @@ class Auth:
         except Exception:
             return None
 
-
     def get_reset_password_token(self, email: str) -> str:
         """retuns a token"""
-        user = self._db.find_user_by(email=email)
-        if user:
-            uid = str(uuid.uuid4())
-            user.reset_token = uuid
-        else:
-            raise ValueError
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                uid = str(uuid.uuid4())
+                user.reset_token = uuid
+            else:
+                raise ValueError
+        except:
+            pass
