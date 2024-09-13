14bccb2d16d2c2c29239735f3229e4c659349e6e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 13:07:51 +0000

    the profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,15 +63,17 @@ def logout():
 
 
 @app.route("/profile", methods=["GET"], strict_slashes=False)
-def profile():
-    """the profile endpoint"""
+def profile() -> str:
+    """GET /profile
+    Return:
+        - The user's profile information.
+    """
     session_id = request.cookies.get("session_id")
-    if session_id is None:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user is None:
-            abort(403)
-        return jsonify({"email": user.email}), 200
-    abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
