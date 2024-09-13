dcbdc13d57b2da03ba0c8832d30e0bca58c5a171
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:37:41 +0000

    the logout view created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,17 +49,18 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
-    try:
-        session_id = request.cookies.get("session_id")
-        if session_id:
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        try:
             user = AUTH.get_user_from_session_id(session_id)
             if user:
                 AUTH.destroy_session(user.id)
                 return redirect("/")
             else:
                 abort(403)
-    except Exception:
-        abort(403)
+        except Exception:
+            abort(403)
+    abort(403)
     
 
 
