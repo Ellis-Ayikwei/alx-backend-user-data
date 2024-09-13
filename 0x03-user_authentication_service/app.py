72c23eae8e2ebcf73ca3db4400df9b5818ff9d6e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:12:07 +0000

    edited the app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,13 +46,14 @@ def login():
             abort(401)
     abort(401)
 
+
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for('index'))
+        return redirect("/")
     return make_response("", 403)
 
 
