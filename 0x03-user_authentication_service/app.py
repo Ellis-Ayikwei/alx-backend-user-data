3fe2be4c98af2320e06e3998f25f15f8cdbd9505
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:34:37 +0000

    added an exception handling

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,15 +49,16 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
-    session_id = request.cookies.get("session_id")
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-            return redirect("/", code=302)
-        else:
-            abort(403)
-    else:
+    try:
+        session_id = request.cookies.get("session_id")
+        if session_id:
+            user = AUTH.get_user_from_session_id(session_id)
+            if user:
+                AUTH.destroy_session(user.id)
+                return redirect("/", code=302)
+            else:
+                abort(403)
+    except Exception:
         abort(403)
     
 
