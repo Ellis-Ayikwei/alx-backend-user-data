e29e763c27bf6f75db184e1f832bdd2284957c1c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 13:30:04 +0000

    if the user existed - exception handling taken out

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,11 +100,10 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """retuns a token"""
-        try:
-            user = self._db.find_user_by(email=email)
-            if user:
-                reset_token = str(uuid.uuid4())
-                self._db.update_user(user.id, reset_token=reset_token)
-            raise ValueError
-        except NoResultFound:
-            raise ValueError
+        user = self._db.find_user_by(email=email)
+        if user:
+            reset_token = str(uuid.uuid4())
+            self._db.update_user(user.id, reset_token=reset_token)
+        raise ValueError
+        
+           
