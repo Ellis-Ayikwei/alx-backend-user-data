3ee395216b8486232b6bd86f7dcbc1208c10fb20
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:40:17 +0000

    the abort 403

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,17 +51,17 @@ def login():
 def logout():
     session_id = request.cookies.get("session_id")
     if session_id is None:
-        return make_response("", 403)
+        abort(403)
 
     try:
         user_id = AUTH.get_user_from_session_id(session_id).id
     except Exception:
-        return make_response("", 403)
+        abort(403)
 
     try:
         AUTH.destroy_session(user_id)
     except Exception:
-        return make_response("", 403)
+        abort(403)
 
     return redirect("/")
     
