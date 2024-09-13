c36b7eccfb45e26de3c57107bb6af1a510a1d093
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:07:30 +0000

    edited the logout function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def login():
 def logout():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user is not None:
+    if user:
         AUTH.destroy_session(user.id)
         return redirect(url_for('index'))
     return make_response("", 403)
