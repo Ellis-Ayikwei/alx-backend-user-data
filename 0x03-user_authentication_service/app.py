eda250e5472db156a6e5c804da06d7d6866cc07b
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 12:55:34 +0000

    edited the over indentation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,9 +66,9 @@ def logout():
 def profile():
     session_id = request.cookies.get("session_id")
     if session_id:
-            user = AUTH.get_user_from_session_id(session_id)
-            if user is not None:
-                return jsonify({"email": user.email}), 200
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is not None:
+            return jsonify({"email": user.email}), 200
     return abort(403)
 
 
