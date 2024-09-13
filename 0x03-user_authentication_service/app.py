c109236e0a004c46fb511e97724e4b9e2b5eb34c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 12:57:25 +0000

    refactored the code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,11 +65,14 @@ def logout():
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile():
     session_id = request.cookies.get("session_id")
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user is not None:
-            return jsonify({"email": user.email}), 200
-    return abort(403)
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
