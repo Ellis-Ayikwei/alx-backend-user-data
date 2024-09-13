8c9ede4075103f294b5a6402c558b827dfabffa2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 11:33:12 +0000

    the logout route set up

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,16 +49,17 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
-        session_id = request.cookies.get("session_id")
-        if session_id:
-            user = AUTH.get_user_from_session_id(session_id)
-            if user:
-                AUTH.destroy_session(user.id)
-                redirect("/", code=302)
-            else:
-                abort(403)
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/", code=302)
         else:
             abort(403)
+    else:
+        abort(403)
+    
 
 
 @app.route("/profile", methods=["GET"], strict_slashes=False)
