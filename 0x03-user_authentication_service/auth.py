11a73e608f22916fd2323c2f44dfbc9e1da41af4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-13 13:29:01 +0000

    edited the condition of the user , if the user exists raise a value error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -105,5 +105,6 @@ class Auth:
             if user:
                 reset_token = str(uuid.uuid4())
                 self._db.update_user(user.id, reset_token=reset_token)
+            raise ValueError
         except NoResultFound:
             raise ValueError
